bb3f3f8e7ee843470516c859c1d53b980f921eb0
AlphaClone Dev <dev@alphaclone.tech>
2026-04-07 10:44:34 +0200

    perf: remove http image remote pattern (mixed content + SSRF risk)

--- next.config.ts
+++ next.config.ts
@@ -35,10 +35,6 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: '**',
       },
-      {
-        protocol: 'http',
-        hostname: '**',
-      },
     ],
   },
   compiler: {
